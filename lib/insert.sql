73a2d71e9a207dc8598c13225b6ce3fb6d9c9379
Christopher Jones <cjones999983@gmail.com>
2018-06-06 00:10:39 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+("Title 1", "Category 1", 10, "01/01/2000", "12/31/2000"),
+("Title 1", "Category 1", 10, "01/01/2000", "12/31/2000"),
+("Title 1", "Category 1", 10, "01/01/2000", "12/31/2000"),
+("Title 1", "Category 1", 10, "01/01/2000", "12/31/2000"),
+("Title 1", "Category 1", 10, "01/01/2000", "12/31/2000"),
+("Title 1", "Category 1", 10, "01/01/2000", "12/31/2000"),
+("Title 1", "Category 1", 10, "01/01/2000", "12/31/2000"),
+("Title 1", "Category 1", 10, "01/01/2000", "12/31/2000"),
+("Title 1", "Category 1", 10, "01/01/2000", "12/31/2000"),
+("Title 1", "Category 1", 10, "01/01/2000", "12/31/2000"),
